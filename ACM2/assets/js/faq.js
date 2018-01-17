1cbed17bc1654ec2700fb9c5ee8217104d06040e
Chia Hua Lu <15913503+ChiaHuaLu@users.noreply.github.com>
2018-01-17 16:15:38 -0600

    FAQ js finished

---
+++ ACM2/assets/js/faq.js
@@ -0,0 +1,43 @@
+var public_spreadsheet_url = 'https://docs.google.com/spreadsheets/d/1RFAdIn5Lz2t0axlfTqKpwsG3kdY_e_7wMkcQ8kZ_wIc/edit?usp=sharing';
+
+function init() {
+Tabletop.init( { key: public_spreadsheet_url,
+                 callback: showInfo,
+                 simpleSheet: true } );
+}
+
+window.addEventListener('DOMContentLoaded', init)
+
+var unhiddenAnswer = "";
+
+function showInfo(data) {
+	var questions = [];
+	var answers = [];
+	var index = 0;
+	while (data[index] != null && !(data[index].Question == "" || data[index].Answer == "")) {
+		questions[index] = data[index].Question;
+		answers[index] = data[index].Answer;
+		index++;
+	}
+	var numberOfEntries = index;
+	var faq = '<h2>Frequently Asked Questions</h2><div style="padding:0px 5%">';
+	for (var index = 0; index < numberOfEntries; index++) {
+		faq += '<h1 onClick="unhideAnswer(' + index + ')" style="cursor: pointer;">' + questions[index] + '</h1>';
+		faq += '<p id="answer' + index + '" class="hideAnswer">' + answers[index] + '</p>';
+	}
+	document.getElementById("dynamic").innerHTML = faq;
+
+
+}
+
+function unhideAnswer(number) {
+	var answerID = "answer" + number;
+	if (answerID != unhiddenAnswer) {
+		document.getElementById(answerID).classList.remove("hideAnswer"); 
+		if (unhiddenAnswer != "")
+			document.getElementById(unhiddenAnswer).classList.add("hideAnswer");
+		unhiddenAnswer = answerID;
+	}
+	
+
+}
