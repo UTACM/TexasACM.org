7ec4ca4c6fdedfb94b47689b9a1de9ab6e5f0dd7
Chia Hua Lu <thegoldflute@users.noreply.github.com>
2018-01-25 19:25:58 -0600

    FAQ.js and FORMS.js Commented and Cleaned

--- ACM2/assets/js/faq.js
+++ ACM2/assets/js/faq.js
@@ -1,3 +1,4 @@
+// Address of the Google Sheets Database
 var public_spreadsheet_url = 'https://docs.google.com/spreadsheets/d/1RFAdIn5Lz2t0axlfTqKpwsG3kdY_e_7wMkcQ8kZ_wIc/edit?usp=sharing';
 
 function init() {
@@ -6,31 +7,37 @@ Tabletop.init( { key: public_spreadsheet_url,
                  simpleSheet: true } );
 }
 
-window.addEventListener('DOMContentLoaded', init)
-
+window.addEventListener('DOMContentLoaded', init)	// Calls method init when Sheets has loaded
 var unhiddenAnswer = "";
 
+// Method that gets called when data has been pulled from Google Sheets
 function showInfo(data) {
 	var questions = [];
 	var answers = [];
 	var index = 0;
+	//Takes the data from Google Sheets and builds an array of Questions and an array of Answers
 	while (data[index] != null && !(data[index].Question == "" || data[index].Answer == "")) {
 		questions[index] = data[index].Question;
 		answers[index] = data[index].Answer;
 		index++;
 	}
+
 	var numberOfEntries = index;
+	// Builds the HTML code from the Questions and Answers arrays
 	var faq = '<h2>Frequently Asked Questions</h2><div style="padding:0px 5%">';
 	for (var index = 0; index < numberOfEntries; index++) {
 		faq += '<h1 class="faq_question" onClick="unhideAnswer(' + index + ')">' + questions[index] + '</h1>';
 		faq += '<p id="answer' + index + '" class="hideAnswer">' + answers[index] + '</p>';
 	}
 	faq += '<center><a style="border-bottom: none" href="https://docs.google.com/spreadsheets/d/1RFAdIn5Lz2t0axlfTqKpwsG3kdY_e_7wMkcQ8kZ_wIc/edit#gid=0"><button class="button admin">Edit</button></a></center>';
+	// Injects the built HTML code into the div Dynamic
 	document.getElementById("dynamic").innerHTML = faq;
-
-
 }
 
+// When a FAQ Question gets clicked on, this method will hide the currently displaying answer (if any), and 
+// Unhide the answer corresponding to the clicked on answer.
+// If the currently displaying answer is the same as the answer corresponding to the clicked on question,
+// it will be hidden and no new answer will be unhidden
 function unhideAnswer(number) {
 	var answerID = "answer" + number;
 	if (answerID != unhiddenAnswer) {
--- ACM2/assets/js/forms.js
+++ ACM2/assets/js/forms.js
@@ -1,3 +1,4 @@
+// Address of the Google Sheets Database
 var public_spreadsheet_url = 'https://docs.google.com/spreadsheets/d/1wwobshcqPMDMPzTL9G1k2cT6bHfVnyw2He2gCFcuBxI/edit?usp=sharing';
 
 function init() {
@@ -6,15 +7,17 @@ Tabletop.init( { key: public_spreadsheet_url,
                  simpleSheet: true } );
 }
 
-window.addEventListener('DOMContentLoaded', init)
+window.addEventListener('DOMContentLoaded', init)	// Calls method init when Sheets has loaded
 
+// Method that gets called when data has been pulled from Google Sheets
 function showInfo(data) {
-		//var numberOfForms = data[0].NumFormsTotal;
 	var forms = [];
 	var addresses = [];
 	var found = false;
+	// Desired Form = query string, aka string after "?" in url
 	var desiredForm = (window.location.href).substring((window.location.href).indexOf('?') + 1 );
 	var index = 0;
+	//Builds the Forms and Addresses arrays using the Google Sheets Data
 	while (data[index] != null && !(data[index].Address == "" && data[index].Name == "") && !found) {
 		forms[index] = data[index].Name;
 		addresses[index] = data[index].Address;
@@ -24,28 +27,41 @@ function showInfo(data) {
 		index++;
 	}
 	var numberOfForms = index;
+
+	// If the desired form doesn't exist, or no form is selected as desired
 	if (!found) {
-		var table = '<h1 style="text-align:left;">Forms</h1><div id="forms-table-div" style="width:30%; margin:auto; text-align:center;" ><table id="forms-table" style="text-align:left;"><tr><th>List of Available Forms</th></tr>'
+		var table = "";
+		// If user wants to select a form, but form doesn't exist in Database, say "Form not found."
+		if (desiredForm != "" && window.location.href.indexOf('?') != -1) {
+			table += "<p>The form <strong>" + desiredForm + "</strong> cannot be found. Here is a list of all forms:</p>";
+		}
+
+		// Builds the Table of all forms. 
+		table += '<h1 style="text-align:left;">Forms</h1><div id="forms-table-div" style="width:30%; margin:auto; text-align:center;" ><table id="forms-table" style="text-align:left;"><tr><th>List of Available Forms</th></tr>'
 		for (index = 0; index < numberOfForms; index++) {
 			if (forms[index] != "" && addresses[index] != "")
 			table += "<tr><td><a href='"+"form.html?"+forms[index] + "'>" + forms[index] + "</td></a></td></tr>";
 		}
-		table += '</table><a style="border-bottom: none" href="https://docs.google.com/spreadsheets/d/1wwobshcqPMDMPzTL9G1k2cT6bHfVnyw2He2gCFcuBxI/edit#gid=0"><button class="button admin">Edit Forms List</button></a></div>';
-		if (desiredForm != "" && window.location.href.indexOf('?') != -1) {
-			table = "<p>The form <strong>" + desiredForm + "</strong> cannot be found. Here is a list of all forms:</p>" + table;
-		}
+		table += '</table>';
+		// Adds the Edit button under the table
+		table += '<a style="border-bottom: none" href="https://docs.google.com/spreadsheets/d/1wwobshcqPMDMPzTL9G1k2cT6bHfVnyw2He2gCFcuBxI/edit#gid=0"><button class="button admin">Edit Forms List</button></a>';
+		table += '</div>';
+		
 		document.getElementById("dynamic").innerHTML = table;	        	
 		document.title = "All Forms | Texas ACM";
 	}
-	else {
+
+	// If the desired form exists in Database
+	else {	
+		// If the desired form exists in Database, but no address is available, display Unavailable
 		if (addresses[index-1]=="") {
 			document.getElementById("dynamic").innerHTML = "<h1>Sorry, Form not available</h1><p>The ACM staff has not specified the form " + desiredForm +"'s address. Please notify an officer and try again later. Thanks</p>";	  
 			document.title = "Form not Available | Texas ACM";
 		}
+		// If the desired form exists in Database, and address exists, then show the form in an iframe
 		else {
 			document.getElementById("dynamic").innerHTML = "<iframe frameborder='0' scrolling='yes' width='100%' height='1000vh' src='"+ addresses[index-1]+"'></iframe>";	  
 			document.title = desiredForm + " | Texas ACM";
-
 		}
 	}
 }
