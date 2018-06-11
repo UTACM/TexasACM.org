675960eaffd11728a8ebcb30b529d28737ae3abc
rolsanmordua <rosamodu@gmail.com>
2018-06-11 16:47:09 -0400

    Progress in making navbar dynamic with dropdowns
    Not rendering properly yet...

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,30 +1,61 @@
-	// Use of this Script Requires the Tabletop.js Library. The Calling HTML File must include tabletop.js
+// Use of this Script Requires the Tabletop.js Library. The Calling HTML File must include tabletop.js
 
 // Address of the Google Sheets Database
-var public_spreadsheet_url_navbar = 'https://docs.google.com/spreadsheets/d/1E4FiRe8tG_8qWenpKSUrWRVwLIjwQsocyG-wCrrN7tY/edit?usp=sharing';
+var public_spreadsheet_url_navbar = 'https://docs.google.com/spreadsheets/d/1KrnAgk7K1xZY5YKLBqA3mzfyBRP8WKxbM6D2czn5QG8/edit?usp=sharing';
 
 function init() {
-Tabletop.init( { key: public_spreadsheet_url_navbar,
-                 callback: showInfoNav,
-                 simpleSheet: true } );
-}
+  Tabletop.init( { key: public_spreadsheet_url_navbar,
+    callback: showInfoNav,
+    simpleSheet: true } );
+  }
 
-window.addEventListener('DOMContentLoaded', init)	// Calls method init when Sheets has loaded
+  window.addEventListener('DOMContentLoaded', init)	// Calls method init when Sheets has loaded
 
-// Method that gets called when data has been pulled from Google Sheets
-function showInfoNav(data) {
-	var navbarContent = "";
-	//Builds the Forms and Addresses arrays using the Google Sheets Data
+  // Method that gets called when data has been pulled from Google Sheets
+  function showInfoNav(data) {
+    var index = 0; // Line 2 is index 0
+    var navbarContent = '<nav id="nav"><ul>';
+    //Builds the Forms and Addresses arrays using the Google Sheets Data
+    var navbarItemName = [];
+    var itemURL = [];
+    var url = '';
+    var parsedURL = '';
+    var firstOccur = 0;
+    var topMenu = true;
+    while (data[index] != null) {
+      navbarItemName[index] = data[index].NAVBAR_ITEM_NAME;
+      itemURL[index] = data[index].ITEM_URL;
 
+      url = data[index].ITEM_URL;
+      if (index > -1) {
+        var higherIndex = itemURL.indexOf(url.substring(0, url.indexOf('#')));
+        if (url.includes("#")) {
+          // window.alert("This is the index of the upper menu item: " + higherIndex);
+          if (topMenu == true) {
+            navbarContent+='<li><a href="#" class="submenu fa-angle-down">' + navbarItemName[higherIndex] + '</a><ul><li><a href="' + itemURL[index] + '">' + navbarItemName[index] + '</a></li>';
+            topMenu = false;
+          } else {
+            navbarContent+= '<li><a href="' + itemURL[index] + '">' + navbarItemName[index] +'</a></li>';
+          }
+          index++;
+        } else {
+          navbarContent+='</ul></li>';
+          if (index == higherIndex) {
+            index++;
+          } else {
+            navbarContent += '<li><a href="'
+            + itemURL[index] + '">'
+            + navbarItemName[index]
+            + '</a></li>';
+            index++;
+          }
+        }
+      }
+      // navbarContent += '</ul></nav>';
+      //
+      // document.getElementById("navbarContainer").innerHTML = navbarContent;
+    }
+    navbarContent += '</ul></nav>';
 
-		// Writes HTML code based on Form responses
-		// alert(webURL[index] == undefined);
-		// link = ""
-		// link = webURL[index];
-
-		navbarContent += '<nav id="nav" style="z-index: 10000;"><ul><li><a href="index.html">Home</a></li><li><a href="about.html">About</a></li><li><a href="#" class="submenu fa-angle-down">Membership</a><ul><li><a href="membership.html#benefits">Benefits</a></li><li><a href="membership.html#events">Evenets</a></li><li><a href="membership.html#lockers">Lockers</a></li></ul></li><li><a href="faq.html">FAQ</a></li><li><a href="events.html">Events</a></li><li><a href="http://projects.texasacm.org">Projects</a></li><li><a href="corporate.html">Corporate</a></li><li><a href="forms.html">Forms</a></li></ul></nav>';
-
-
-	
-	document.getElementById("navbarContainer").innerHTML = navbarContent;
-}
+    document.getElementById("navbarContainer").innerHTML = navbarContent;
+  }
