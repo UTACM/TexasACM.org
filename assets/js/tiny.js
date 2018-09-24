1043e220f8f06318e273d2d7c252ae65701d1c12
ChiaHuaLu <thegoldflute@gmail.com>
2018-09-24 18:19:12 -0500

    Implement TinyUrl

--- assets/js/tiny.js
+++ assets/js/tiny.js
@@ -15,79 +15,56 @@ window.addEventListener('DOMContentLoaded', init)	// Calls method init when Shee
 function showInfo(data) {
 	var nameColumnTitle = "Name\n(Must be 1 word)";
 	var addressColumnTitle = "Address\n(include http:// if external link)";
-	var forms = [];
+	var links = [];
 	var addresses = [];
-	var imbedInACM = [];
 	var found = false;
-	// Desired Form = query string, aka string after "?" in url
-	var desiredForm = (window.location.href).substring((window.location.href).indexOf('?') + 1 );
+	// Desired link = query string, aka string after "?" in url
+	var desiredlink = (window.location.href).substring((window.location.href).indexOf('?') + 1 );
 	var index = 0;
-	//Builds the Forms and Addresses arrays using the Google Sheets Data
-	while (data[index] != null && !(data[index][addressColumnTitle] == "" && data[index][nameColumnTitle] == "") && !found) {
-		forms[index] = data[index][nameColumnTitle];
+	//Builds the links and Addresses arrays using the Google Sheets Data
+	while (data[index] != undefined && !(data[index][addressColumnTitle] == "" && data[index][nameColumnTitle] == "") && !found) {
+		links[index] = data[index][nameColumnTitle];
 		addresses[index] = data[index][addressColumnTitle];
-		imbedInACM[index] = embedInACMorNot(data[index].EmbedInACM);
-		if (forms[index].toLowerCase()==desiredForm.toLowerCase() && desiredForm != "") {
+		if (links[index] != undefined && links[index].toLowerCase()==desiredlink.toLowerCase() && desiredlink != "") {
 			found = true;
 		}
 		index++;
 	}
-	var numberOfForms = index;
+	var numberOflinks = index;
 
-	// If the desired form doesn't exist, or no form is selected as desired
+	// If the desired link doesn't exist, or no link is selected as desired
 	if (!found) {
 		var table = "";
-		// If user wants to select a form, but form doesn't exist in Database, say "Form not found."
-		if (desiredForm != "" && window.location.href.indexOf('?') != -1) {
-			table += "<p>The form <strong>" + desiredForm + "</strong> cannot be found. Here is a list of all forms:</p>";
+		// If user wants to select a link, but link doesn't exist in Database, say "link not found."
+		if (desiredlink != "" && window.location.href.indexOf('?') != -1) {
+			table += "<p>The link <strong>" + desiredlink + "</strong> cannot be found. Here is a list of all links:</p>";
 		}
 
-		// Builds the Table of all forms. 
-		table += '<h1 style="text-align:left;">Forms</h1><div id="forms-table-div" style="width:30%; margin:auto; text-align:center;" ><table id="forms-table" style="text-align:left;"><tr><th>List of Available Forms</th></tr>'
-		for (index = 0; index < numberOfForms; index++) {
-			if (forms[index] != "" && addresses[index] != "") 
-				table += "<tr><td><a href='"+forms[index] + "'>" + forms[index] + "</td></a></td></tr>";
+		// Builds the Table of all links. 
+		table += '<h1 style="text-align:left;">Links</h1><div id="links-table-div" style="width:30%; margin:auto; text-align:center;" ><table id="links-table" style="text-align:left;"><tr><th>List of Available links</th></tr>'
+		for (index = 0; index < numberOflinks; index++) {
+			if (links[index] != "" && addresses[index] != "") 
+				table += "<tr><td><a href='"+addresses[index] + "'>" + links[index] + "</td></a></td></tr>";
 		}
 		table += '</table>';
 		// Adds the Edit button under the table
-		table += '<a style="border-bottom: none" href="https://docs.google.com/spreadsheets/d/1wwobshcqPMDMPzTL9G1k2cT6bHfVnyw2He2gCFcuBxI/edit#gid=0"><button class="button admin">Edit Forms List</button></a>';
+		table += '<a style="border-bottom: none" href="https://docs.google.com/spreadsheets/d/1bHF_1i9F031XrSvlw4g41X4-0ZfG9mpvWiqLdKHh4j0/edit#gid=0"><button class="button admin">Edit links List</button></a>';
 		table += '</div>';
 		
 		document.getElementById("dynamic").innerHTML = table;	        	
 		document.title = "All Urls | Texas ACM";
 	}
 
-	// If the desired form exists in Database
+	// If the desired link exists in Database
 	else {	
-		// If the desired form exists in Database, but no address is available, display Unavailable
+		// If the desired link exists in Database, but no address is available, display Unavailable
 		if (addresses[index-1]=="") {
-			document.getElementById("dynamic").innerHTML = "<h1>Sorry, Form not available</h1><p>The ACM staff has not specified the form " + desiredForm +"'s address. Please notify an officer and try again later. Thanks</p>";	  
+			document.getElementById("dynamic").innerHTML = "<h1>Sorry, link not available</h1><p>The ACM staff has not specified the link " + desiredlink +"'s address. Please notify an officer and try again later. Thanks</p>";	  
 			document.title = "Url not Available | Texas ACM";
 		}
-		// If the desired form exists in Database
+		// If the desired link exists in Database
 		else {
-			// if EmbedInACM is true, embed the form in an iFrame
-			if (imbedInACM[index-1]) {
-				document.getElementById("dynamic").innerHTML = "<iframe frameborder='0' scrolling='yes' width='100%' height='1000vh' src='"+ addresses[index-1]+"'></iframe>";	  
-				document.title = desiredForm + " | Texas ACM";
-			}
-			// if EmbedInACM is false, imbedInACM directly to URL
-			else {
-				window.location.replace(addresses[index-1]);
-			}
+			window.location.replace(addresses[index-1]);
 		}
 	}
-}
-
-//Checks the cell to see if it says "no"
-function embedInACMorNot(cell) {
-	if (!cell=="") {
-		if (cell.charAt(0)=='N'||cell.charAt(0)=='n')
-			return false;
-		else
-			return true;
-	} else {
-		return true;
-	}
-
 }
