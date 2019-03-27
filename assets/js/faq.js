adb0c94f42607e75e01ff99fc3aa57d12afb851f
rolsanmordua <rosamodu@gmail.com>
2019-03-26 20:01:36 -0500

    Made faq.js vars constants

--- assets/js/faq.js
+++ assets/js/faq.js
@@ -4,8 +4,8 @@
 var public_spreadsheet_url = 'https://docs.google.com/spreadsheets/d/1RFAdIn5Lz2t0axlfTqKpwsG3kdY_e_7wMkcQ8kZ_wIc/edit?usp=sharing';
 
 // Column Names from Google Sheets Database
-questionsColumn = "Question";
-answersColumn = "Answer";
+let questionsColumn = "Question";
+let answersColumn = "Answer";
 
 window.addEventListener('DOMContentLoaded', init)	// Calls method init when Sheets has loaded
 
@@ -37,14 +37,14 @@ function buildFAQTable(data) {
 	return content;
 }
 
-// When a FAQ Question gets clicked on, this method will hide the currently displaying answer (if any), and 
+// When a FAQ Question gets clicked on, this method will hide the currently displaying answer (if any), and
 // Unhide the answer corresponding to the clicked on answer.
 // If the currently displaying answer is the same as the answer corresponding to the clicked on question,
 // it will be hidden and no new answer will be unhidden
 function unhideAnswer(number) {
 	var answerID = "answer" + number;
 	if (answerID != unhiddenAnswer) {
-		document.getElementById(answerID).classList.remove("hideAnswer"); 
+		document.getElementById(answerID).classList.remove("hideAnswer");
 	}
 	if (unhiddenAnswer != "")
 		document.getElementById(unhiddenAnswer).classList.add("hideAnswer");
